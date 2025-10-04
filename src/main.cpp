9d0378b56b7f2fd980f3113a870eb2b89e458562
Bui Van Yen <buivanyen215@gmail.com>
2025-10-04 12:25:36 +0700

    Drive motor tien lui dung

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
-
+//Part 3 : Drive the motor
+//Motor Terminal 1 --> motor driver output1
+//Motor Terminal 2 --> motor driver output2'
+//power supply 12V --> motor driver Vin/GND
+//Motor driver gnd --> arduino GND
+//Motor driver PWMA input --> arduino pin 5
+//Motor driver AIN1 input --> arduino pin 7
+//Motor driver AIN2 input --> arduino pin 6
 #define ENCA 2 //Yellow wire
 #define ENCB 3 //white wire
+#define PWM 5 
+#define IN1 7
+#define IN2 6
 
 int pos=0;
 
@@ -14,8 +24,30 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(pos);
+    setMotor(1, 25, PWM, IN1, IN2); //quay tiến
+    delay(200);
+    Serial.println(pos);
+    setMotor(-1, 25, PWM, IN1, IN2); //quay lùi
+    delay(200);
+    Serial.println(pos);
+    setMotor(0, 0, PWM, IN1, IN2); //dừng
+    delay(200);
+    Serial.println(pos);
 }
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm, pwmVal);
+    if(dir==1){
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+    }else if(dir==-1){
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+    }else{
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, LOW);
+    }
+}
+
 //trên cạnh lên của A, nếu B = 1 thì đang quay một chiều, B = 0 thì chiều ngược lại
 void readEncoder(){
   int b = digitalRead(ENCB);
